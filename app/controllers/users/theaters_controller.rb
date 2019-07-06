69a046a7b03aae711c9d9cfbd66cef8d55acc22b
ishiharakotomi <ishihara.5103@gmail.com>
2019-07-06 08:09:01 +0000

    userのtheater/index,showを作成

--- app/controllers/users/theaters_controller.rb
+++ app/controllers/users/theaters_controller.rb
@@ -1,4 +1,10 @@
 class Users::TheatersController < ApplicationController
 	def index
+    	@theaters = Theater.all
+    	@theaters = Theater.page(params[:page]).reverse_order
+	end
+
+	def show
+    	@theater = Theater.find(params[:id])
 	end
 end
